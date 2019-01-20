674c3d006ba2c736c2e2094680ab11d07be0aca0
Rachel Morelli <rachel@withstarling.com>
2019-01-19 20:42:32 -0800

    hero text edits

--- src/components/HeroMessage/index.js
+++ src/components/HeroMessage/index.js
@@ -5,8 +5,8 @@ import './styles.css';
 const HeroMessage = ({ openRSVP }) => {
   return (
     <div class='hero-message'>
-      <div class='hero-message-title'>Josh & Rachel</div>
-      <div class='hero-message-date'>09.01.19</div>
+      <div class='hero-message-title'>We're getting married</div>
+      <div class='hero-message-date'>09.01.19 / Pittsburgh, Pennsylvania</div>
       <Button onClick={openRSVP} className='hero-button'>{'RSVP'}</Button>
     </div>
   );
