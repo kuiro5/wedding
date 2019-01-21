86de3aca5c5141ac8fcfc0216f06999d30661f75
Josh Kuiros <joshkuiros@gmail.com>
2019-01-20 20:45:14 -0800

    RSVP stylin

--- src/components/RSVP/index.js
+++ src/components/RSVP/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
+import Button from '../Button';
 import './styles.css';
 
 class RSVP extends Component {
@@ -10,23 +11,50 @@ class RSVP extends Component {
   render() {
     const { isOpen } = this.props;
 
+    const customStyles = {
+      content : {
+        top                   : '50%',
+        left                  : '50%',
+        right                 : 'auto',
+        bottom                : 'auto',
+        marginRight           : '-50%',
+        transform             : 'translate(-50%, -50%)'
+      }
+    };
+
     return(
       <Modal
         isOpen={isOpen}
+        onRequestClose={this.cancel}
+        style={customStyles}
       >
+        <div className='close' onClick={this.cancel} />
         <h1>RSVP</h1>
         <form class='rsvp-form' action='https://formspree.io/joshkuiros@gmail.com' method='POST'>
           <div class='rsvp-label-input'>
-            <label for='name'>Name </label>
-            <input id='name' name='name' placeholder='Enter your name' required/>
+            <div>Email*</div>
+            <input id='email' name='email' required/>
           </div>
           <div class='rsvp-label-input'>
-            <label for='name'>Guests </label>
-            <input id='guests' name='guests' type='number' min='1' value='1' required/>
+            <div>Name*</div>
+            <input id='name' name='name' required/>
+            <input id='name' name='name' required/>
+          </div>
+          <div class='rsvp-label-input'>
+            <div>Are you coming?*</div>
+            <input id='coming' name='coming' required/>
+          </div>
+          <div class='rsvp-label-input'>
+            <div>Are you bringing anyone with you?</div>
+            <input id='guests' name='guests'/>
+          </div>
+          <div class='rsvp-label-input'>
+            <div>Got any special messages for us?</div>
+            <input type='text' id='messages' name='messages'/>
           </div>
           <div class='rsvp-footer'>
-            <button onClick={this.cancel}>Cancel</button>
-            <button>Save</button>
+            <Button onClick={this.cancel}>Cancel</Button>
+            <Button>Save</Button>
           </div>
         </form>
       </Modal>
