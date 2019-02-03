1be09a529fdf441c6c9a9b37653d63a0acaf7cb5
Josh Kuiros <joshkuiros@gmail.com>
2019-02-03 12:23:20 -0800

    Update Our Story

--- src/components/OurStory/index.js
+++ src/components/OurStory/index.js
@@ -11,7 +11,9 @@ const OurStory = () => {
       <h1>Our Story</h1>
       <h4>Happy Valley to California</h4>
       <div>
-        Josh and Rach first met in college at Penn State. Josh and Rach first met in college at Penn State.
+        <p>Rach and Josh first met their junior year at Penn State. Through hayrides and formals, football games and dollar slices, their relationship blossomed.</p>
+        <p>After graduation, they embarked on their next adventure to California. Arriving in SFO with only a couple suitcases, they found their first apartment and started settling in to life on the west coast. </p>
+        <p>6 years later on Lover's Lane in San Francisco, a city they both love, Josh asked Rach to marry him.</p>
       </div>
     </Box>
     </Flex>
