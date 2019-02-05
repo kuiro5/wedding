ef2f8eb87e63f8a6d24a0beec30ab303327963fe
Rachel Morelli <rachel@withstarling.com>
2019-02-04 22:02:03 -0800

    small text edit

--- src/components/OurStory/index.js
+++ src/components/OurStory/index.js
@@ -12,7 +12,7 @@ const OurStory = () => {
       <h4 className="our-story-header">Happy Valley to California</h4>
       <div>
         <p>Rach and Josh first met their junior year at Penn State. Through hayrides and formals, football games and dollar slices, their relationship blossomed.</p>
-        <p>After graduation, they embarked on their next adventure to California. Arriving in SFO with only a couple suitcases, they found their first apartment and started settling in to life on the west coast. </p>
+        <p>After graduation, they embarked on their next adventure to California. Arriving in SFO with only a few suitcases, they found their first apartment and started settling in to life on the west coast. </p>
         <p>6 years and one short walk later, Josh asked Rach to marry him under the eucalyptus trees in San Francisco, a city where they both have learned to appreciate all the beauty and quirks life has to offer.</p>
       </div>
     </Box>
