e3c42ad6d6e333ca47eb44225b718c4da00cf80d
Rachel Morelli <rachel@withstarling.com>
2019-02-24 23:00:26 -0800

    add time

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -8,17 +8,18 @@ const Details = () => {
       <h4 className='details-header'>The Details</h4>
       <div className='details-section'>
         <h1>Where</h1>
-        <div className="where-title">Ceremony + Reception</div>
-        <div className="where-subtitle">The Pennsylvanian</div>
+        <div className="where-text">The Pennsylvanian</div>
         <div className="where-text">Pittsburgh, Pennsylvania</div>
       </div>
       <div className='details-section'>
         <h1>When</h1>
-        <div>SEPTEMBER 01, 2019</div>
+        <div className="where-text">September 01, 2019</div>
+        <div className="where-text">at 5 o'clock in the evening</div>
+        <div className="where-italic">reception to follow</div>
       </div>
       <div>
         <h1>Attire</h1>
-        <div>COCKTAIL</div>
+        <div className="where-text">Cocktail</div>
       </div>
   </Flex>
   );
