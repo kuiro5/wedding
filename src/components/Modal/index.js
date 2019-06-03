8c89ff824258ad0373aa79e022a49b00189a188a
Josh Kuiros <joshkuiros@gmail.com>
2019-06-02 21:30:43 -0700

    New modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,38 +1,46 @@
 import React, { Component } from 'react';
-import ReactModal from 'react-modal';
+import ReactModal from 'react-aria-modal';
 import { MdClose } from 'react-icons/md';
 import './styles.css';
 
 class Modal extends Component {
 
+  getApplicationNode = () => {
+    return document.getElementById('___gatsby');
+  }
   render() {
     const { children, isOpen, title, close } = this.props;
 
-    const customStyles = {
-      content: {
-        padding: '3rem',
-        borderRadius: '8px',
-        boxShadow: '0 10px 20px rgba(0,0,0,0.10)',
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-        minWidth: '50%',
-        maxHeight: '90vh',
-        overflowY: 'auto',
-      }
+    if(!isOpen) {
+      return null;
+    }
+
+    const modalStyles = {
+      padding: '3rem',
+      borderRadius: '8px',
+      boxShadow: '0 10px 20px rgba(0,0,0,0.10)',
+      minWidth: '50%',
+      maxHeight: '90vh',
+      overflowY: 'auto',
+      backgroundColor: 'white',
+      position: 'relative',
+    };
+
+    const underlayStyles = {
+      background: 'rgba(255, 255, 255, .6)',
     };
 
     return(
       <ReactModal
-        isOpen={isOpen}
-        onRequestClose={close}
-        style={customStyles}
+        titleText={title}
+        onExit={close}
+        getApplicationNode={this.getApplicationNode}
+        dialogStyle={modalStyles}
+        underlayStyle={underlayStyles}
+        verticallyCenter
       >
-        <MdClose className='close' onClick={close} />
         <h1>{title}</h1>
+        <MdClose className='close' onClick={close} />
         {children}
       </ReactModal>
     );
